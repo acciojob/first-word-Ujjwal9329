197c2a307c465c86514dd73a2a93c59502fff96a
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-06 11:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function firstWord(s) {
-  // your code here
+  return s.split(" ")[0]; // Split the string by space and return the first part
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
